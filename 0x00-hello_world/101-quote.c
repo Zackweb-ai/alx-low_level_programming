0660dc1a2a2896df019c25ebcfa6ee10c2dfea18
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-13 08:50:30 -0700

    task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 /**
  *  main - Entry point
- *  Description: "prints exactly and that piece of art is useful\" - Dora Korpar,
- *  2015-10-19\" followed by a new line " using unistd library
+ *  Description: "prints exactly and that piece of art is useful\" -
+ *  Dora Korpar,2015-10-19\" followed by a new line " using unistd library
  *  Return: 1 (error)
  */
 int main(void)
@@ -11,4 +11,4 @@ int main(void)
 
 	write(1, quot, 59);
 	return (1);
-}	
+}
