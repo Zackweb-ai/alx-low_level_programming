3cea90451af5d0694b4b44c0c01c36d2ab908c4d
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-13 08:42:26 -0700

    task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ *  main - Entry point
+ *  Description: "prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19\"
+ *  followed by a new line " using unistd library
+ *  Return: 1 (error)
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,quote,59);
+	return (1);
+}	
