7f17f0a201a378bca592f797f883f0b8be8c31fc
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-13 07:41:15 -0700

    task 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  *main - Entry Point
- *Description: "prints the size of various types on the computer 
+ *Description: "prints the size of various types on the computer
  * it is compiled and run on"
  * Return: Always 0 (Succsess)
  */
