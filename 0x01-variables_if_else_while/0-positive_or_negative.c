6daa49bb3a125da114ecc526dd26420fb1986431
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-14 06:34:22 -0700

    task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,13 +11,13 @@ int main(void)
 
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
-		if (n>=1)
+		if (n>1)
 		{
 			printf("%d is positive\n",n);
 		}
 		else
 		{
-			if (n<=0)
+			if (n<0)
 			{
 				printf("%d is negative\n",n);
 			}
