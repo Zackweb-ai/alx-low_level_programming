c0c31b32f6cbee7977f65cbc68dcf9c52ba57452
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-14 08:10:58 -0700

    task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*prints all single digit numbers of base 10 starting from 0,
+*followed by a new line.
+* followed by a new line.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int my_digit = 0;
+
+	while (my_digit <= 9)
+	{
+		printf("%d", my_digit);
+		my_digit++;
+	}
+	printf('\n');
+	return (0);
+}
