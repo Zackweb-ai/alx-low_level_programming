93c49d2fd9cf7600270c84dba8f504dd9cf39631
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-14 07:34:04 -0700

    task 3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
  * main - Entry point
  * a program that prints the alphabet in lowercase, followed by a new line.      
  * Return: Always 0 (Success)
- *       */
+*/
 int main(void)
 {
 	char my_char = 'a';
