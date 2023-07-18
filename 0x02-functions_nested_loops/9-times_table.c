68bb6fdedf52f00c971db12556938a405ee10f96
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-18 10:31:26 -0700

    task 10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,12 @@ void times_table(void)
 {
 	int num, multi, pd;
 
-	for (num =0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
-		for (multi =1; multi <= 9; multi++)
+		for (multi = 1; multi <= 9; multi++)
 		{
-			_putchar('.');
+			_putchar(',');
 			_putchar(' ');
 
 			pd = num * multi;
