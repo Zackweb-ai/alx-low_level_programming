88854f5ad040db1e3416549cffb028716aeda756
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-18 11:11:41 -0700

    task 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @n1: first input
+ * @n2: second input
+ *
+ * Return: sum of n1 and n2
+ */
+
+int add(int n1, int n2)
+{
+	int sum = n1 + n2;
+	
+	return (sum)
+}
