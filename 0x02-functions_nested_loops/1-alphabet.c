9eba5a0c5e1cd4112948c24bb02b1f66e307af10
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-18 06:51:39 -0700

    task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
+ * prints the aplhabet a - z
+ * from a - z
  * print_alphabet : uses the _putchar function to print
  * the alphabet, in lowercase, followed by a new line.
- *
  */
 
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
