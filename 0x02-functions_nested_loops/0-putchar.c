e1fc9fe9968302978dc58b38d0cbf76fac085b1f
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-18 06:37:49 -0700

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* imain - Entry point
+* main - Entry point
 * Description: prints _putchar, followed by a new line.
 * Return: Always 0 (Success)
 */
@@ -9,6 +9,7 @@ int main(void)
 {
 	char my_char[] = "_putchar";
 	int i;
+
 	for (i = 0 ; my_char[i] != '\0'; i++)
 	{
 		putchar(my_char[i]);
