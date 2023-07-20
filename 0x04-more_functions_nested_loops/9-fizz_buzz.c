651ffd21a1a5aba99caaf1e9fe2863ee61d36818
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-20 08:43:00 -0700

    task 10

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,25 +11,25 @@ int main(void)
 {
 	int num;
 
-    	for (num = 1; num <= 100; num++)
-    	{
-        if (num % 3 == 0 && num % 5 != 0)
-        {
-            printf("Fizz ");
-        }
-        else if (num % 5 == 0 && num % 3 != 0)
-        {
-            printf("Buzz ");
-        }
-        else if (num % 3 == 0 && num % 5 == 0)
-        {
-            printf("FizzBuzz ");
-        }
-        else
-        {
-            printf("%d ", num);
-        }
-    }
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0 && num % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		else if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
 
 	printf("\n");
 }
