7691306c1ebea3030982c2f977de08b9b31113a5
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-20 08:22:24 -0700

    task 9

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,11 +8,21 @@
 void print_square(int size)
 {
 	int r, c;
-
-	for (r = 1; r <= size; r++)
+	
+	if (size <= 0)
 	{
-		for (c = 1; c <= size; c++)
-			_putchar('#');
 		_putchar('\n');
+	} else
+	{
+		for (r = 1; r <= size; r++)
+		{
+			for (c = 1; c <= size; c++)
+			{
+				_putchar('#');
+			}
+		
+			_putchar('\n');
+		}
 	}
+
 }
