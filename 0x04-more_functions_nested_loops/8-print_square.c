a1cb795aa45c1700b89984ce07c5250b2ca4a2b2
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-20 08:14:19 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line;
+ * @size: the size of the square
+ */
+
+void print_square(int size)
+{
+	int r, c;
+
+	for (r = 1; r <= size; r++)
+	{
+		for (c = 1; c <= size; column++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
