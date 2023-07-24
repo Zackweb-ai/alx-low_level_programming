3aef0d6965b96d95399da8e6d1e83eb5a4542fdc
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-24 07:11:29 -0700

    task 4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,12 +5,11 @@
  * @str: string to print
  */
 
-void _puts(char *str)
+int void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str + 0);
-		str++;
+		_putchar(*str++);
 	}
 		_putchar('\n');
 }
