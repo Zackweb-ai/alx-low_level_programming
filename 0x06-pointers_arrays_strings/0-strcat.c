39895ae60e3d6acdd0140aee9c343ee0b66063d4
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-26 07:05:18 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int s1, s2;
+
+	s = 0;
+	while (dest[c])
+		s++;
+	for (s2 = 0; src[s2]; s2++)
+		dest[s++] = src[s2];
+
+	return (dest);
+}
