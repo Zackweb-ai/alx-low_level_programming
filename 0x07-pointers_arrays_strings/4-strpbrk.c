62af7c2bbe1ca28964c25b874b2799fbdb6476a4
Zackweb-ai <zari.zakaria@gmail.com>
2023-07-31 16:39:05 -0700

    task 5

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,18 +11,17 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-		int i;
+	int i;
 
-		while (*s)
-	i	{
-			for (i = 0; accept[i]; i++)
-			{
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
 			if (*s == accept[i])
 			return (s);
-			}
-		s++;
 		}
+		s++;
+	}
 
 	return ('\0');
 }
-
