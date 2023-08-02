7e6c328535b1ff09b3dcb021eaa7617e020007f2
Zackweb-ai <zari.zakaria@gmail.com>
2023-08-02 06:58:20 -0700

    task 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * Return: the square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
+ * actual_sqrt_recursion -  finds the square root
+ * @n: number
+ * @in: current integer being checked
+ * Return: the result
+ */
+
+int square(int n, int in)
+{
+	if (in * in < n)
+		return (square(n, in + 1));
+	if (in * in == n)
+		return (n);
+	else
+		return (-1);
+}
+
+
