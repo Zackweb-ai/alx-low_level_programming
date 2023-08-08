c343f7bd743eed3202b12002a7d7b84030ddf835
Zackweb-ai <zari.zakaria@gmail.com>
2023-08-08 05:37:13 -0700

    task 1

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer 
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int a, b;	
+	char *c;
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	a = b = 0;
+	while (s1[a] != '\0')
+		a++;
+	while (s2[b] != '\0')
+		b++;
+	c = malloc(sizeof(char) * (a + b + 1));
+
+	if (conct == NULL)
+		return (NULL);
+	a = b = 0;
+	while (s1[a] != '\0')
+	{
+		c[a] = s1[a];
+		a++;
+	}
+
+	while (s2[b] != '\0')
+	{
+		c[a] = s2[b];
+		a++, b++;
+	}
+	c[a] = '\0';
+	return (c);
+}
