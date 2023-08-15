676ac8ba3bd6b10d7db93c5028b387e32d03f295
Zackweb-ai <zari.zakaria@gmail.com>
2023-08-15 04:38:15 -0700

    task 2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,15 +5,14 @@
  * @d: dog to initialize
  * @name: name
  * @age: age
- * @owner: owner 
+ * @owner: owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
 }
-
