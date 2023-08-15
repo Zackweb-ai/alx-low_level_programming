b374885aeb8ff50e54cba2768f3dcf4f38ad72e9
Zackweb-ai <zari.zakaria@gmail.com>
2023-08-15 04:25:53 -0700

    task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - the dog's info
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's owner name
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
