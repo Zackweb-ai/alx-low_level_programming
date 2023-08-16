3d3aa102cb85b9e23c694fa369ba651d09723c02
Zackweb-ai <zari.zakaria@gmail.com>
2023-08-16 16:13:08 -0700

    Task 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,7 +3,7 @@
 
 /**
  * array_iterator -  executes a function given as
- a parameter on each element of an array
+ * a parameter on each element of an array
  * @array: array
  * @size: size of the array
  * @action: pointer to the function you need to use
