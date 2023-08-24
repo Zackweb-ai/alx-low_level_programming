0b106664d343d710971ebf76fbd4d347af4b21ce
Zackweb-ai <zari.zakaria@gmail.com>
2023-08-24 10:17:36 -0700

    Task 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,7 +5,7 @@
 /**
  * add_node - adds a new node at the beginning of a list
  * @head: double pointer to head node
- * @str: new string 
+ * @str: new string
  *
  * Return: the address of the new element, or NULL if it fails
  */
