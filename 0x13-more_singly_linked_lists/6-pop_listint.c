fe62e57d2f1967f75de746687c94f44d9f18247a
Zackweb-ai <zari.zakaria@gmail.com>
2023-08-29 16:34:23 -0700

    Tasks

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,10 +2,14 @@
 
 /**
  * pop_listint - Deletes the head node of a linked list.
- * @head: A pointer to the pointer of the first element in the linked list.
- * Return: The data inside the element that was deleted, or 0 if the list is empty.
- * Description: This function removes the first node (head) from the linked list,
- * freeing its memory and updating the head pointer to point to the next node.
+ * @head: A pointer to the pointer of the first element
+ * in the linked list.
+ * Return: The data inside the element that was deleted,
+ * or 0 if the list is empty.
+ * Description: This function removes the first node (head)
+ * from the linked list,
+ * freeing its memory and updating the head pointer to
+ * point to the next node.
  * If the list is empty, it returns 0.
  */
 
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - Retrieves the node at a specified index in a linked list.
+ * get_nodeint_at_index - Retrieves the node at a
+ * specified index in a linked list.
  * @head: A pointer to the first node in the linked list.
  * @index: The index of the node to retrieve.
  * Return: A pointer to the node at the specified index, or NULL if not found.
  * Description: This function traverses the linked list from the beginning
- * to the specified index and returns a pointer to the node found at that index.
+ * to the specified index and returns a pointer to the node found at that index
  * If the index is out of range or the list is empty, it returns NULL.
  */
 
