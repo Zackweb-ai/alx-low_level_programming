16bc4d43c23a9fe3d4cd569bd4a8dc0de8714bcb
Zackweb-ai <zari.zakaria@gmail.com>
2023-08-31 16:08:47 -0700

    tasks

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,10 +3,13 @@
 /**
  * set_bit - Sets a bit at a specified index to 1 in an unsigned long integer.
  * @n: A pointer to the number in which to set the bit.
- * @index: The index of the bit to set to 1 (0 being the least significant bit).
+ * @index: The index of the bit to set to 1
+ * (0 being the least significant bit).
  *
- * This function takes a pointer to an unsigned long integer and sets the bit at
- * the specified index to 1. It first checks if the index is valid (0 to 63) and
+ * This function takes a pointer to an unsigned
+ * long integer and sets the bit at
+ * the specified index to 1. It first checks
+ * if the index is valid (0 to 63) and
  * then uses bitwise operations to set the bit.
  *
  * @n: A pointer to the unsigned long integer to modify.
@@ -17,9 +20,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    if (index > 63)
-        return (-1);
+	if (index > 63)
+		return (-1);
 
-    *n = ((1UL << index) | *n);
-    return (1);
+	*n = ((1UL << index) | *n);
+		return (1);
 }
